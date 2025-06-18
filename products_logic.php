340ac21829ad36b6276b4098f0551295ea45d7b1
EMEZETA29 <roberto.pobletet@correoaiep.cl>
2025-06-18 17:18:34 -0400

    Agregada carpeta icons / Modificacion diseño

--- products_logic.php
+++ products_logic.php
@@ -1,8 +1,20 @@
 <?php
 $products = [
-    ["id" => 1, "name" => "Pelota de Fútbol", "price" => 15000, "image" => "img/futbol.jpg"],
-    ["id" => 2, "name" => "Balón de Básquetbol", "price" => 18000, "image" => "img/basquet.jpg"],
-    ["id" => 3, "name" => "Raqueta de Tenis", "price" => 30000, "image" => "img/tenis.jpg"],
-    ["id" => 4, "name" => "Guantes de Boxeo", "price" => 25000, "image" => "img/boxeo.jpg"]
+    ["id" => 1, "name" => "Pelota Adidas Oficial", "price" => 19990, "image" => "img/pelota_adidas.jpg"],
+    ["id" => 2, "name" => "Botines Nike Mercurial", "price" => 59990, "image" => "img/botines_nike.jpg"],
+    ["id" => 3, "name" => "Camiseta Selección Chile", "price" => 34990, "image" => "img/camiseta_chile.jpg"],
+    ["id" => 4, "name" => "Canilleras Deportivas", "price" => 9990, "image" => "img/canilleras.jpg"],
+    ["id" => 5, "name" => "Guantes de Arquero Puma", "price" => 27990, "image" => "img/guantes_puma.jpg"],
+    ["id" => 6, "name" => "Balón Entrenamiento Umbro", "price" => 14990, "image" => "img/balon_umbro.jpg"],
+    ["id" => 7, "name" => "Polera Térmica Negra", "price" => 12990, "image" => "img/polera_termica.jpg"],
+    ["id" => 8, "name" => "Short de Fútbol Adidas", "price" => 14990, "image" => "img/short_adidas.jpg"],
+    ["id" => 9, "name" => "Medias Deportivas Largas", "price" => 4990, "image" => "img/medias_largas.jpg"],
+    ["id" => 10, "name" => "Petos de Entrenamiento (6u)", "price" => 11990, "image" => "img/petos.jpg"],
+    ["id" => 11, "name" => "Bolso Deportivo Fútbol", "price" => 18990, "image" => "img/bolso_deportivo.jpg"],
+    ["id" => 12, "name" => "Set de Conos de Entrenamiento", "price" => 10990, "image" => "img/conos_entrenamiento.jpg"],
+    ["id" => 13, "name" => "Chaleco Reflectante Técnico", "price" => 7990, "image" => "img/chaleco_reflectante.jpg"],
+    ["id" => 14, "name" => "Bomba de Aire Manual", "price" => 4990, "image" => "img/bomba_aire.jpg"],
+    ["id" => 15, "name" => "Silbato con Cordón", "price" => 2990, "image" => "img/silbato.jpg"],
+    ["id" => 16, "name" => "Arco Plegable Portátil", "price" => 89990, "image" => "img/arco_plegable.jpg"]
 ];
 ?>
--- products_view.php
+++ products_view.php
@@ -7,6 +7,29 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header class="navbar">
+    <div class="logo">DEPORTIVA</div>
+    
+    <nav class="menu">
+        <ul>
+            <li><a href="#">Categorías</a></li>
+            <li><a href="#">Hombre</a></li>
+            <li><a href="#">Mujer</a></li>
+            <li><a href="#">Niños</a></li>
+            <li><a href="#">Accesorios</a></li>
+            <li><a href="#">Salud</a></li>
+        </ul>
+    </nav>
+
+    <div class="search-user">
+        <input type="text" placeholder="Buscar...">
+        <div class="icons">
+            <a href="#"><img src="icons/user.svg" alt="Usuario" /></a>
+            <a href="#"><img src="icons/cart.svg" alt="Carrito" /></a>
+        </div>
+    </div>
+</header>
+
     <div class="product-grid">
         <?php foreach ($products as $product): ?>
             <div class="product-card">
